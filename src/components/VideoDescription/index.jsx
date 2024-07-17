ff130431c6dfeee9d41ecadd6e3571131f80ce8f
Francisco Rodriguez <fran11799@outlook.com>
2024-07-17 15:56:38 +0200

    feat(Root): Add SongTicker to VideoDescription

--- src/components/VideoDescription/index.jsx
+++ src/components/VideoDescription/index.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import AlbumDisk from "./AlbumDisk";
+import SongTicker from "../SongTicker/index";
 import styles from "./styles.module.css";
 
-const VideoDescription = ({ author, description, albumCover }) => {
+const VideoDescription = ({ author, description, albumCover, songTitle }) => {
   return (
     <footer className={styles.description}>
       <div className={styles.textWrapper}>
-        <strong>
-          <a href={`/user/${author}`} className={styles.author}>
-            @{author}
-          </a>
-        </strong>
-        <p className={styles.text}>{description}</p>
+        <section>
+          <strong>
+            <a href={`/user/${author}`} className={styles.author}>
+              @{author}
+            </a>
+          </strong>
+          <p className={styles.text}>{description}</p>
+        </section>
+
+        <SongTicker songTitle={songTitle} />
       </div>
 
       <div>

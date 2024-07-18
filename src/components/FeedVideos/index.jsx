509b47c7dbd1ac7de5a95fa26078a4048a778510
Francisco Rodriguez <fran11799@outlook.com>
2024-07-18 20:04:09 +0200

    feat(Root): Add another video to FeedVideos

--- src/components/FeedVideos/index.jsx
+++ src/components/FeedVideos/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import VideoPlayer from "../VideoPlayer/index.jsx";
 import video from "../../../assets/descarga2.mp4";
 import video2 from "../../../assets/descarga3.mp4";
-import video3 from "../../../assets/descarga.mp4";
+import video3 from "../../../assets/descarga4.mp4";
 import styles from "./styles.module.css";
 import getVideos from "../../services/index.js";
 
@@ -27,12 +27,26 @@ const VIDEOS = [
     avatar: "https://avatars.githubusercontent.com/u/138864214?v=4",
     description:
       "Spain winning semifinals euro 24 🤌🏼⚽️🇪🇸🥹 #spain #champions #campeones #euro2024 #winners #football",
-    likes: 1079,
-    shares: 259,
-    comments: 234,
+    likes: 2079,
+    shares: 545,
+    comments: 383,
     songTitle: "La Roja Baila - Himno Oficial",
     albumCover: "https://ichef.bbci.co.uk/images/ic/1024x576/p03yydym.jpg",
     src: video,
+  },
+  {
+    id: 3,
+    username: "fr4n0m",
+    avatar: "https://avatars.githubusercontent.com/u/138864214?v=4",
+    description:
+      "Alonso X Lobato 2023 Season #Alonso #Lobato #f1 #f1noticias #f1edit",
+    likes: 3333,
+    shares: 333,
+    comments: 333,
+    songTitle: "sonido original - Antonio Lobato",
+    albumCover:
+      "https://cdn.mobygames.com/ca057d3c-ac08-11ed-b950-02420a00012d.webp",
+    src: video3,
   },
 ];
 
@@ -51,15 +65,19 @@ export default function FeedVideos() {
     return <span>{error}</span>;
   }
 
-  return videos.map((video) => {
+  return VIDEOS.map((video) => {
     /*VIDEOS.map Para que se muestren los videos locales*/
     const { user = {} } = video;
     const { avatar, username } = user;
 
     return (
       <div key={video.id} className={styles.item}>
-        <VideoPlayer {...video} username={username} avatar={avatar} />
-        {/* {video.username} Para que aparezca el nombre de usuario en local*/}
+        <VideoPlayer
+          {...video}
+          username={video.username}
+          avatar={video.avatar}
+        />
+        {/* {video.username} {video.username} Para que aparezca el nombre de usuario en local*/}
       </div>
     );
   });

e8384b6584104da5cddca0407b0d61249a9feafd
Francisco Rodriguez <fran11799@outlook.com>
2024-07-18 17:13:27 +0200

    feat(Root): Add supabase config files

---
+++ src/services/index.js
@@ -0,0 +1,18 @@
+import supabase from "./supabase";
+
+const getVideos = async () => {
+  const { data, error } = await supabase
+    .from("videos")
+    .select(
+      `*, user:user_id (
+        avatar,
+        username,
+        id
+      )`
+    )
+    .order("created_at", { ascending: false });
+
+  return [error, data];
+};
+
+export default getVideos;

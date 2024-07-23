853be44e8a5220b9c1dc74cf501199604c26b57b
Francisco Rodriguez <fran11799@outlook.com>
2024-07-23 14:28:42 +0200

    feat(Root): Add upload video path to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import "./App.css";
+import { Route } from "wouter";
+import Upload from "./pages/Upload/index.jsx";
 import FeedVideos from "./components/FeedVideos/index.jsx";
 
 const App = () => {
   return (
     <div className="App">
       <main>
-        <FeedVideos />
+        <Route path="/">
+          <FeedVideos />
+        </Route>
+
+        <Route path="/upload">
+          <Upload />
+        </Route>
       </main>
     </div>
   );

8fb59b14d0ed6a45530504d33b8b4aade5e51a1a
Francisco Rodriguez <fran11799@outlook.com>
2024-07-23 14:27:45 +0200

    feat(Root): Add uploadVideo and publishVideo functions to supabase index

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,33 @@
 import supabase from "./supabase";
 
+const uploadVideo = async ({ video }) => {
+  const { data, error } = await supabase.storage
+    .from("videos")
+    .upload("videos/", video);
+  if (error) {
+    console.log(error);
+  }
+  console.log(data);
+};
+
+const publishVideo = async ({ videoSrc, description }) => {
+  const defaultAlbum =
+    "https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/81fd9366b0f1841e1e6990492b5c9004~c5_720x720.jpeg?x-expires=1632060000&x-signature=lJ7S3aE3YqHr8WbhkwXNIasqkZo%3D";
+  const defaultSong = "fr4n0m songs";
+
+  const { data, error } = await supabase.from("videos").insert([
+    {
+      user_id: "",
+      description,
+      albumCover: defaultAlbum,
+      songTitle: defaultSong,
+      src: videoSrc,
+    },
+  ]);
+
+  return [error, data];
+};
+
 const getVideos = async () => {
   const { data, error } = await supabase
     .from("videos")
@@ -15,4 +43,4 @@ const getVideos = async () => {
   return [error, data];
 };
 
-export default getVideos;
+export default { getVideos, uploadVideo, publishVideo };

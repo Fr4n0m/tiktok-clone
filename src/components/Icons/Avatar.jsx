da675736ad8042971bc2e526b12dca5b1da6a4f8
Francisco Rodriguez <fran11799@outlook.com>
2024-07-24 14:20:38 +0200

    fix(Root): Change Avatar img

--- src/components/Icons/Avatar.jsx
+++ src/components/Icons/Avatar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const AvatarIcon = (props) => {
-  return <img src="assets/logo.png" {...props}></img>;
+  return (
+    <img
+      src="https://avatars.githubusercontent.com/u/138864214?v=4"
+      {...props}
+    ></img>
+  );
 };
 
 export default AvatarIcon;

d437dbc5503c3099a9eb193c7d6c17560f3c2902
Francisco Rodriguez <fran11799@outlook.com>
2024-07-24 14:11:45 +0200

    fix(Root): Change friends page import

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import NavBar from "./components/NavBar";
 const Home = lazy(() => import("./pages/Home/index.jsx"));
 const Upload = lazy(() => import("./pages/Upload/index.jsx"));
 const Login = lazy(() => import("./pages/Login/index.jsx"));
-const Friends = lazy(() => import("./pages/Login/index.jsx"));
+const Friends = lazy(() => import("./pages/Friends/index.jsx"));
 const Inbox = lazy(() => import("./pages/Inbox/index.jsx"));
 const Profile = lazy(() => import("./pages/Profile/index.jsx"));
 

ad8803ed027ec860e9f0ecf704bac5aa55ef6b65
Francisco Rodriguez <fran11799@outlook.com>
2024-07-24 13:56:59 +0200

    fix(Root): Add index.jsx to imports pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import "./App.css";
 import { Route } from "wouter";
 import NavBar from "./components/NavBar";
 
-import Home from "./pages/Home";
-import Upload from "./pages/Upload";
-import Login from "./pages/Login";
-import Friends from "./pages/Friends";
-import Inbox from "./pages/Inbox";
-import Profile from "./pages/Profile";
+import Home from "./pages/Home/index.jsx";
+import Upload from "./pages/Upload/index.jsx";
+import Login from "./pages/Login/index.jsx";
+import Friends from "./pages/Friends/index.jsx";
+import Inbox from "./pages/Inbox/index.jsx";
+import Profile from "./pages/Profile/index.jsx";
 
 const App = () => {
   return (

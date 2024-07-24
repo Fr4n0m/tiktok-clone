271bc498616f329e71c192cd5a84680244c6cfd9
Francisco Rodriguez <fran11799@outlook.com>
2024-07-24 14:12:11 +0200

    fix(Root): Avatar img doesn't look good

--- src/components/Icons/Avatar.jsx
+++ src/components/Icons/Avatar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const AvatarIcon = (props) => {
-  return <img src="/assets/logo.png" {...props}></img>;
+  return <img src="assets/logo.png" {...props}></img>;
 };
 
 export default AvatarIcon;
